a315bc3aad79b05641b64db9ad5b424501066e44
suhaib <selramly22@yahoo.com>
2026-04-29 20:32:11 +0200

    style: add @extends PHPDoc to BrandFactory to match UserFactory

--- aroma-api/database/factories/BrandFactory.php
+++ aroma-api/database/factories/BrandFactory.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+/** @extends Factory<Brand> */
 class BrandFactory extends Factory
 {
     protected $model = Brand::class;
